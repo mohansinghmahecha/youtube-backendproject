fb83330ae37e4d31f7bc74fcfc4abf129ade8947
Mohan Singh Mahecha <mohansinghmahecha2000@gmail.com>
2024-09-01 01:42:12 +0530

    netify

---
+++ netlify-functions/getSubscribers.js
@@ -0,0 +1,25 @@
+const { MongoClient } = require('mongodb');
+require('dotenv').config(); // If using environment variables
+
+exports.handler = async (event, context) => {
+  const client = new MongoClient("mongodb+srv://mohansinghmahecha2000:uRyluGTuCIQXjefE@cluster0.ue1ll.mongodb.net/youtube-backend");
+
+  try {
+    await client.connect();
+    const database = client.db('youtube-backend');
+    const collection = database.collection('subscribers');
+    const subscribers = await collection.find({}).toArray();
+    
+    return {
+      statusCode: 200,
+      body: JSON.stringify(subscribers),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Unable to fetch data' }),
+    };
+  } finally {
+    await client.close();
+  }
+};
