01d735670b4e3b2327941c35a7aa4400bd1d52a5
Mohan Singh Mahecha <mohansinghmahecha2000@gmail.com>
2024-09-02 01:42:42 +0530

    netify solve 2

--- netlify-functions/getSubscribers.js
+++ netlify-functions/getSubscribers.js
@@ -1,23 +1,20 @@
-// Import the MongoClient class from the 'mongodb'const { MongoClient } = require('mongodb');
 const { MongoClient } = require('mongodb');
 
-// Define the handler function which is the entry point for AWS Lambda or similar serverless functions.
 exports.handler = async (event, context) => {
-
-  // connect to the MongoDB database.
-  // MongoDB Atlas URI.
   const client = new MongoClient("mongodb+srv://mohansinghmahecha2000:uRyluGTuCIQXjefE@cluster0.ue1ll.mongodb.net/youtube-backend");
-
+  
   try {
-      // Connect to mongodb server.
     await client.connect();
-    //our database name
     const database = client.db('youtube-backend');
-    //our database collection
     const collection = database.collection('subscribers');
-    //fet all and convert to array 
-    const subscribers = await collection.find({}).toArray();
-    
+    let subscribers;
+
+    if (event.path.endsWith('/names')) {
+      subscribers = await collection.find({}, { projection: { name: 1, subscribedChannel: 1 } }).toArray();
+    } else {
+      subscribers = await collection.find({}).toArray();
+    }
+
     return {
       statusCode: 200,
       body: JSON.stringify(subscribers),
